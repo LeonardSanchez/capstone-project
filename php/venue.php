f183f3c6dcaaba72243d23d82b783055074fc05b
Leonard <Suavelen69@gmail.com>
2014-11-21 11:44:39 -0700

    Profile updated

--- php/venue.php
+++ php/venue.php
@@ -1,4 +1,4 @@
- <?php
+<?php
 /**
  * mySQL Enabled Event
  *
@@ -55,18 +55,19 @@ class Venue
 	/**
 	 * constructor for Venue
 	 *
-	 * @param int $newVenueId venue id (or null if new object)
-	 * @param string $newVenueName venue name
-	 * @param int $newVenueCapacity venue capacity
-	 * @param string $newVenuePhone venue phone
-	 * @param string $newVenueWebsite venue website
+	 * @param int    $newVenueId       venue id (or null if new object)
+	 * @param string $newVenueName     venue name
+	 * @param int    $newVenueCapacity venue capacity
+	 * @param string $newVenuePhone    venue phone
+	 * @param string $newVenueWebsite  venue website
 	 * @param string $newVenueAddress1 venue address first line
 	 * @param string $newVenueAddress2 venue address second line
-	 * @param string $newVenueCity venue city
-	 * @param string $newVenueState venue state
-	 * @param string $newVenueZipCode venue zip code
+	 * @param string $newVenueCity     venue city
+	 * @param string $newVenueState    venue state
+	 * @param string $newVenueZipCode  venue zip code
 	 */
-	public function __construct($newVenueId, $newVenueName, $newVenueCapacity, $newVenuePhone, $newVenueWebsite, $newVenueAddress1, $newVenueAddress2, $newVenueCity, $newVenueState, $newVenueZipCode) {
+	public function __construct($newVenueId, $newVenueName, $newVenueCapacity, $newVenuePhone, $newVenueWebsite, $newVenueAddress1, $newVenueAddress2, $newVenueCity, $newVenueState, $newVenueZipCode)
+	{
 		try {
 			$this->setVenueId($newVenueId);
 			$this->setVenueName($newVenueName);
@@ -92,7 +93,8 @@ class Venue
 	 *
 	 * @return int venue id (or null if new object)
 	 */
-	public function getVenueId() {
+	public function getVenueId()
+	{
 		return ($this->venueId);
 	}
 
@@ -103,7 +105,8 @@ class Venue
 	 * @throws UnexpectedValueException if not an integer or null
 	 * @throws RangeException if venue id isn't positive
 	 */
-	public function setVenueId($newVenueId) {
+	public function setVenueId($newVenueId)
+	{
 		// zeroth, set all the venue id to be null if a new object
 		if($newVenueId === null) {
 			$this->venueId = null;
@@ -130,7 +133,8 @@ class Venue
 	 *
 	 * @return string value of venue name
 	 */
-	public function getVenueName() {
+	public function getVenueName()
+	{
 		return ($this->venueName);
 	}
 
@@ -140,7 +144,8 @@ class Venue
 	 * @param string $newVenueName venue name
 	 * @throws UnexpectedValueException if it doesn't appear to be a venue name
 	 */
-	public function setVenueName($newVenueName) {
+	public function setVenueName($newVenueName)
+	{
 		// sanitize the Venue Name as a likely Venue Name
 		$newVenueName = trim($newVenueName);
 		if(($newVenueName = filter_var($newVenueName, FILTER_SANITIZE_STRING)) == false) {
@@ -156,7 +161,8 @@ class Venue
 	 *
 	 * @return int value of venue capacity
 	 */
-	public function getVenueCapacity() {
+	public function getVenueCapacity()
+	{
 		return ($this->venueCapacity);
 	}
 
@@ -167,7 +173,8 @@ class Venue
 	 * @throws UnexpectedValueException if not an integer
 	 * @throws RangeException if venue capacity isn't positive
 	 */
-	public function setVenueCapacity($newVenueCapacity) {
+	public function setVenueCapacity($newVenueCapacity)
+	{
 
 		// first, ensure the venue capacity is an integer
 		if(filter_var($newVenueCapacity, FILTER_VALIDATE_INT) === false) {
@@ -189,7 +196,8 @@ class Venue
 	 *
 	 * @return string value of venue phone
 	 */
-	public function getVenuePhone() {
+	public function getVenuePhone()
+	{
 		return ($this->venuePhone);
 	}
 
@@ -197,11 +205,12 @@ class Venue
 	 * sets string value of venue phone
 	 *
 	 * @param string $newVenuePhone venue phone
-	 * @param string strip out all special characters using sanitize
+	 * @param        string         strip out all special characters using sanitize
 	 * @throws UnexpectedValueException if the input doesn't appear to be a phone number
 	 * @throws RangeException when input is not a valid phone number format
 	 */
-	public function setVenuePhone($newVenuePhone) {
+	public function setVenuePhone($newVenuePhone)
+	{
 		// sanitize the VenuePhone as a likely phone number
 		$newVenuePhone = trim($newVenuePhone);
 
@@ -222,7 +231,8 @@ class Venue
 	 *
 	 * @return string value of venue website
 	 */
-	public function getVenueWebsite() {
+	public function getVenueWebsite()
+	{
 		return ($this->venueWebsite);
 	}
 
@@ -232,7 +242,8 @@ class Venue
 	 * @param string $newVenueWebsite venue website
 	 * @throws UnexpectedValueException if the input doesn't appear to be an URL
 	 */
-	public function setVenueWebsite($newVenueWebsite) {
+	public function setVenueWebsite($newVenueWebsite)
+	{
 		// sanitize teh VenueWebsite as a likely URL
 		$newVenueWebsite = trim($newVenueWebsite);
 		if(($newVenueWebsite = filter_var($newVenueWebsite, FILTER_VALIDATE_URL)) == false) {
@@ -248,7 +259,8 @@ class Venue
 	 *
 	 * @return string value of address1
 	 */
-	public function getVenueAddress1(){
+	public function getVenueAddress1()
+	{
 		return ($this->venueAddress1);
 	}
 
@@ -258,7 +270,8 @@ class Venue
 	 * @param string $newVenueAddress1 venue address1
 	 * @throws UnexpectedValueException if the input doesn't appear to be an address line 1
 	 */
-	public function setVenueAddress1($newVenueAddress1) {
+	public function setVenueAddress1($newVenueAddress1)
+	{
 		//sanitize the VenueAddress1 as a likely address line 1
 		$newVenueAddress1 = trim($newVenueAddress1);
 		if(($newVenueAddress1 = filter_var($newVenueAddress1, FILTER_SANITIZE_STRING)) == false) {
@@ -274,7 +287,8 @@ class Venue
 	 *
 	 * @return string value of venueAddress2
 	 */
-	public function getVenueAddress2(){
+	public function getVenueAddress2()
+	{
 		return ($this->venueAddress2);
 	}
 
@@ -284,7 +298,8 @@ class Venue
 	 * @param string $newVenueAddress2 venue address2
 	 * @throws UnexpectedValueException if the input doesn't appear to be an address line 2
 	 */
-	public function setVenueAddress2($newVenueAddress2) {
+	public function setVenueAddress2($newVenueAddress2)
+	{
 		//sanitize the VenueAddress2 as a likely address line 1
 		$newVenueAddress2 = trim($newVenueAddress2);
 		if(($newVenueAddress2 = filter_var($newVenueAddress2, FILTER_SANITIZE_STRING)) == false && !empty($newVenueAddress2)) {
@@ -300,7 +315,8 @@ class Venue
 	 *
 	 * @return string value of venueCity
 	 */
-	public function getVenueCity(){
+	public function getVenueCity()
+	{
 		return ($this->venueCity);
 	}
 
@@ -310,7 +326,8 @@ class Venue
 	 * @param string $newVenueCity venue city
 	 * @throws UnexpectedValueException if the input doesn't appear to be a city
 	 */
-	public function setVenueCity($newVenueCity) {
+	public function setVenueCity($newVenueCity)
+	{
 		//sanitize the VenueCity as a likely city
 		$newVenueCity = trim($newVenueCity);
 		if(($newVenueCity = filter_var($newVenueCity, FILTER_SANITIZE_STRING)) == false) {
@@ -326,7 +343,8 @@ class Venue
 	 *
 	 * @return string value of venueState
 	 */
-	public function getVenueState(){
+	public function getVenueState()
+	{
 		return ($this->venueState);
 	}
 
@@ -336,7 +354,8 @@ class Venue
 	 * @param string $newVenueState venue state
 	 * @throws UnexpectedValueException if the input doesn't appear to be a 2 character state
 	 */
-	public function setVenueState($newVenueState) {
+	public function setVenueState($newVenueState)
+	{
 		//sanitize the VenueState as a likely US state
 		$newVenueState = trim($newVenueState);
 		if(($newVenueState = filter_var($newVenueState, FILTER_SANITIZE_STRING)) == false) {
@@ -352,7 +371,8 @@ class Venue
 	 *
 	 * @return string value of venueZipCode
 	 */
-	public function getVenueZipCode() {
+	public function getVenueZipCode()
+	{
 		return ($this->venueZipCode);
 	}
 
@@ -362,7 +382,8 @@ class Venue
 	 * @param string $newVenueZipCode venue zip code
 	 * @throws UnexpectedValueException if the input does not appear to be a valid zip code
 	 */
-	public function setVenueZipCode($newVenueZipCode) {
+	public function setVenueZipCode($newVenueZipCode)
+	{
 		//sanitize the VenueZipCode as a likely US zip code
 		$newVenueZipCode = trim($newVenueZipCode);
 		if(($newVenueZipCode = filter_var($newVenueZipCode, FILTER_SANITIZE_STRING)) == false) {
@@ -379,7 +400,8 @@ class Venue
 	 * @param resource $mysqli pointer to mySQL connection, by reference
 	 * @throws mysqli_sql_exception when mySQL related errors occur
 	 */
-	public function insert(&$mysqli) {
+	public function insert(&$mysqli)
+	{
 		// handle degenerate cases
 		if(gettype($mysqli) !== "object" || get_class($mysqli) !== "mysqli") {
 			throw(new mysqli_sql_exception("input is not a mysqli object"));
@@ -414,93 +436,95 @@ class Venue
 		// update the null venueId with what mySQL just gave me
 		$this->venueId = $mysqli->insert_id;
 	}
-		/**
-		 * deletes this Venue from mySQL
-		 *
-		 * @param resource $mysqli pointer to mySQL connection, by reference
-		 * @throws mysqli_sql_exception when mySQL related errors occur
-		 */
-		public function delete(&$mysqli) {
-			// handle degenerate cases
-			if(gettype($mysqli) !== "object" || get_class( $mysqli) !== "mysqli") {
-				throw(new mysqli_sql_exception("input is not a mysqli object"));
-			}
 
-			// enforce the venueId is not null (i.e. don't delete a venue that hasn't been inserted)
-			if($this->venueId === null) {
-				throw(new mysqli_sql_exception("Unable to delete a venue that does not exist"));
-			}
-
-			// create query template
-			$query = "DELETE FROM venue WHERE venueId = ?";
-			$statement = $mysqli->prepare($query);
-			if($statement === false) {
-				throw(new mysqli_sql_exception("Unable to prepare statement"));
-			}
-
-			// bind the member variables to the place holder in the template
-			$wasClean = $statement->bind_param("i", $this->venueId);
-			if($wasClean === false) {
-				throw(new mysqli_sql_exception("Unable to bind parameters"));
-			}
-
-			// execute the statement
-			if($statement->execute() === false) {
-				throw(new mysqli_sql_exception("Unable to execute mySQL statement"));
-			}
+	/**
+	 * deletes this Venue from mySQL
+	 *
+	 * @param resource $mysqli pointer to mySQL connection, by reference
+	 * @throws mysqli_sql_exception when mySQL related errors occur
+	 */
+	public function delete(&$mysqli)
+	{
+		// handle degenerate cases
+		if(gettype($mysqli) !== "object" || get_class($mysqli) !== "mysqli") {
+			throw(new mysqli_sql_exception("input is not a mysqli object"));
 		}
 
-		/**
-		 * update this Venue in mySQL
-		 *
-		 * @param resource $mysqli pointer to mySQL connection, by reference
-		 * @throws mysqli_sql_exception when mySQL related errors occur
-		 */
-		public function update(&$mysqli) {
-			// handle degenerate cases
-			if(gettype($mysqli) !== "object" || get_class($mysqli) !== "mysqli") {
-				throw(new mysqli_sql_exception("input not a mysqli object"));
-			}
-
-			// enforce the venueId is not null (i.e., don't update a venue that hasn't been created
-			if($this->venueId === null) {
-				throw(new mysqli_sql_exception("Unable to update a venue that does not exist"));
-			}
-
-			// create query template
-			$query = "UPDATE venue SET venueName = ?, venueCapacity = ?, venuePhone = ?, venueWebsite = ?, venueAddress1 = ?, venueAddress2 = ?, venueCity = ?, venueState = ?, venueZipCode = ? WHERE venueId = ?";
-			$statement = $mysqli->prepare($query);
-			if($statement === false) {
-				throw(new mysqli_sql_exception("Unable to prepare statement"));
-			}
-
-			// bind the member variables to the place holders in the template
-			$wasClean = $statement->bind_param("sissssssss",	$this->venueName,		$this->venueCapacity,	$this->venuePhone,
-																				$this->venueWebsite,	$this->venueAddress1,	$this->venueAddress2,
-																				$this->venueCity,		$this->venueState,		$this->venueZipCode,
-																				$this->venueId);
-			if($wasClean === false) {
-				throw(new mysqli_sql_exception("Unable to bind parameters"));
-			}
-
-			// execute the statement
-			if($statement->execute() === false) {
-				throw(new mysqli_sql_exception("Unable to execute mySQL statement"));
-			}
+		// enforce the venueId is not null (i.e. don't delete a venue that hasn't been inserted)
+		if($this->venueId === null) {
+			throw(new mysqli_sql_exception("Unable to delete a venue that does not exist"));
 		}
 
+		// create query template
+		$query = "DELETE FROM venue WHERE venueId = ?";
+		$statement = $mysqli->prepare($query);
+		if($statement === false) {
+			throw(new mysqli_sql_exception("Unable to prepare statement"));
+		}
 
+		// bind the member variables to the place holder in the template
+		$wasClean = $statement->bind_param("i", $this->venueId);
+		if($wasClean === false) {
+			throw(new mysqli_sql_exception("Unable to bind parameters"));
+		}
+
+		// execute the statement
+		if($statement->execute() === false) {
+			throw(new mysqli_sql_exception("Unable to execute mySQL statement"));
+		}
+	}
+
+	/**
+	 * update this Venue in mySQL
+	 *
+	 * @param resource $mysqli pointer to mySQL connection, by reference
+	 * @throws mysqli_sql_exception when mySQL related errors occur
+	 */
+	public function update(&$mysqli)
+	{
+		// handle degenerate cases
+		if(gettype($mysqli) !== "object" || get_class($mysqli) !== "mysqli") {
+			throw(new mysqli_sql_exception("input not a mysqli object"));
+		}
+
+		// enforce the venueId is not null (i.e., don't update a venue that hasn't been created
+		if($this->venueId === null) {
+			throw(new mysqli_sql_exception("Unable to update a venue that does not exist"));
+		}
+
+		// create query template
+		$query = "UPDATE venue SET venueName = ?, venueCapacity = ?, venuePhone = ?, venueWebsite = ?, venueAddress1 = ?, venueAddress2 = ?, venueCity = ?, venueState = ?, venueZipCode = ? WHERE venueId = ?";
+		$statement = $mysqli->prepare($query);
+		if($statement === false) {
+			throw(new mysqli_sql_exception("Unable to prepare statement"));
+		}
+
+		// bind the member variables to the place holders in the template
+		$wasClean = $statement->bind_param("sissssssss", $this->venueName, $this->venueCapacity, $this->venuePhone,
+			$this->venueWebsite, $this->venueAddress1, $this->venueAddress2,
+			$this->venueCity, $this->venueState, $this->venueZipCode,
+			$this->venueId);
+		if($wasClean === false) {
+			throw(new mysqli_sql_exception("Unable to bind parameters"));
+		}
+
+		// execute the statement
+		if($statement->execute() === false) {
+			throw(new mysqli_sql_exception("Unable to execute mySQL statement"));
+		}
+	}
 
 
 	/**
 	 * gets the Venue by VenueId
 	 *
-	 * @param resource $mysqli pointer to mySQL connection, by reference
-	 * @param string $venueId venueId to search for
+	 * @param resource $mysqli  pointer to mySQL connection, by reference
+	 * @param string   $venueId venueId to search for
 	 * @return mixed Venue found or null if not found
 	 * @throws mysqli_sql_exception when mySQL related errors occur
 	 */
-	public function getVenueByVenueId(&$mysqli, $venueId) {
+	public function getVenueByVenueId(&$mysqli, $venueId)
+	{
 
 		// handle degenerate cases
 		if(gettype($mysqli) !== "object" || get_class($mysqli) !== "mysqli") {
@@ -514,7 +538,7 @@ class Venue
 
 		// create query template
 		$query = "SELECT venueId, venueName, venueCapacity, venuePhone, venueWebsite, venueAddrss1, venueAddress2, venueCity, venueState, venueZipCode FROM venue WHERE venueId = ?";
-		$statement  = $mysqli->prepare($query);
+		$statement = $mysqli->prepare($query);
 		if($statement === false) {
 			throw(new mysqli_sql_exception("Unable to prepare statement"));
 		}
@@ -545,29 +569,29 @@ class Venue
 		if($row !== null) {
 			try {
 				$venue = new Venue($row["venueId"], $row["venueName"], $row["venueCapacity"], $row["venuePhone"], $row["venueWebsite"], $row["venueAddress1"], $row["venueAddress2"], $row["venueCity"], $row["venueState"], $row["venueZipCode"]);
-			}
-			catch(Exception $exception) {
+			} catch(Exception $exception) {
 				// if the row couldn't be converted, rethrow it
 				throw(new mysqli_sql_exception("Unable to convert row to Venue", 0, $exception));
 			}
 
 			// if we got here, the Venue is good - return it
-			return($venue);
+			return ($venue);
 		} else {
 			// 404 venue not found - return null instead
-			return(null);
+			return (null);
 		}
 	}
 
 	/**
 	 * gets the Venue by Venue Name
 	 *
-	 * @param resource $mysqli pointer to mySQL connection, by reference
-	 * @param string $venueName venueName to search for
+	 * @param resource $mysqli    pointer to mySQL connection, by reference
+	 * @param string   $venueName venueName to search for
 	 * @return Venue if found, or null if not
 	 * @throws mysqli_sql_exception when mySQL related errors occur
 	 */
-	public static function getVenueByVenueName(&$mysqli, $venueName) {
+	public static function getVenueByVenueName(&$mysqli, $venueName)
+	{
 		// handle degenerate cases
 		if(gettype($mysqli) !== "object" || get_class($mysqli) !== "mysqli") {
 			throw(new mysqli_sql_exception("input is not a mysqli object"));
@@ -580,8 +604,8 @@ class Venue
 		}
 
 		// create query template
-		$query 		= "SELECT venueId, venueName, venueCapacity, venuePhone, venueWebsite, venueAddress1, venueAddress2, venueCity, venueState, venueZipCode FROM venue WHERE venueName = ?";
-		$statement	= $mysqli->prepare($query);
+		$query = "SELECT venueId, venueName, venueCapacity, venuePhone, venueWebsite, venueAddress1, venueAddress2, venueCity, venueState, venueZipCode FROM venue WHERE venueName = ?";
+		$statement = $mysqli->prepare($query);
 		if($statement === false) {
 			throw(new mysqli_sql_exception("Unable to prepare statement"));
 		}
@@ -608,22 +632,26 @@ class Venue
 		// 2) if there's no result, we can just return null
 		$row = $result->fetch_assoc(); // fetch_assoc returns a row as an associative array
 
-		// convert the associative array to a Venue
-		if($row !== null) {
+		while(($row = $result->fetch_assoc()) !== null) {
 			try {
-				$venue = new Venue($row["venueId"], $row["venueName"], $row["venueCapacity"], $row["venuePhone"], $row["venueWebsite"], $row["venueAddress1"], $row["venueAddress2"], $row["venueCity"], $row["venueState"], $row["venueZipCode"]);
+				$venue = new Venue($row["venueId"], $row["venueName"], $row["venueCapacity"], $row["venuePhone"], $row["venueWebsite"], $row["venueAddress1"], $row["venueAddress2"], $row["venueCity"], $row["venueCity"], $row["venueState"], $row["venueZipCode"]);
+				$venues[] = $venue;
 			} catch(Exception $exception) {
-				// if the row couldn't be converted, rethrow it
+				// if the row was not able to be converted rethrow
 				throw(new mysqli_sql_exception("Unable to convert row to Venue", 0, $exception));
 			}
+		}
 
-			// if we got here, the venue is good - return it
-			return ($venue);
-		} else {
-			// 404 Venue not found - return null instead
+		$numberOfVenues = count($venues);
+		if($numberOfVenues === 0) {
 			return (null);
+		} else if($numberOfVenues === 1) {
+			return ($venues[0]);
+		} else {
+			return ($venues);
 		}
 	}
+
 
 	/**
 	 * gets the Venue by Venue City
@@ -633,13 +661,13 @@ class Venue
 	 * @return Venue if found, or null if not
 	 * @throws mysqli_sql_exception when mySQL related errors occur
 	 */
+
 	public static function getVenueByVenueCity(&$mysqli, $venueCity)
 	{
 		// handle degenerate cases
 		if(gettype($mysqli) !== "object" || get_class($mysqli !== "mysqli")) {
 			throw(new mysqli_sql_exception("input is not a mysqli object"));
 		}
-
 		// sanitize the Venue City before searching
 		$venueCity = trim($venueCity);
 		if(($venueCity = filter_var($venueCity, FILTER_SANITIZE_STRING)) == false) {
@@ -825,7 +853,6 @@ class Venue
 			return (null);
 		}
 	}
-
 }
 
 ?>
