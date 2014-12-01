d8c8947e7a8f143e0f3b5ce201d35c8549e6904b
James Mistalski <kimomistalski@gmail.com>
2014-12-01 15:47:18 -0700

    - created new venuesearch.php in forms directory for form processor to search by venue name https://app.asana.com/0/19128794071892/19128794071892

--- javascript/venuesearch.js
+++ javascript/venuesearch.js
@@ -1,9 +1,21 @@
-function validateVenueSearch() {
-	var venLookup = document.forms["venueSearch"]["venName"];
-	if(venLookup == "") {
-		alert("Venue Name must be entered");
-		return false;
-	}
-	if
+// validate the form using jQuery
+$(document).ready(function()
+{
+	// setup the form validation
+	$("venueSearch").validate({
+		// debug option in jQuery's validator
+		debug: true,
+		// rules dictate what is (in)valid
+		rules: {
+			venue: {
+				required: true,
+				venue: true
+			}
+		},
 
-}
+		// messages are what are displayed to the user
+		messages: {
+			venue: "Please enter a Venue to search by"
+			}
+	})
+})
--- php/forms/venuesearch.php
+++ php/forms/venuesearch.php
@@ -1,4 +1,21 @@
-<?php
-/**
- * This is a form processor to search upcoming events by venue name
- */ 
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<title>Venue Search</title>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script src="http://malsup.github.com/jquery.form.js"></script>
+		<script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/jquery.validate.min.js"></script>
+		<script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/additional-methods.min.js"></script>
+		<script type="text/javascript" src="venuesearch.js"></script>
+	</head>
+	<body>
+		<form id="venueSearch" method="get" action="venue.php">
+			<label for="venue">Venue</label>
+			<input type="text" id="venue" name="venue" /><br />
+			<button id="search" type="submit">Search</button>
+		</form>
+	</body>
+
+
+</html>
