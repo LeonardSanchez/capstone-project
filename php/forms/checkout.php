7444bd346a3c6fb84df94285bd64f928d2870c46
Brendan Slevin <bslevin87@gmail.com>
2014-12-01 13:04:29 -0700

    added two php forms to start working on

---
+++ php/forms/checkout.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Brendo
+ * Date: 12/1/2014
+ * Time: 1:02 PM
+ */ 
---
+++ php/forms/shopping-cart.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Shopping cart form
+ *
+ * This form will be used to collect quantity of tickets desired and allow the user to see a preview of the order before checkout
+ *
+ * Created by Brendan Slevin
+ */
+
+?>
