464e3157c230f8df49359edef6548cf10a0f2982
Leonard <Suavelen69@gmail.com>
2014-12-03 10:59:57 -0700

    Login/Sign up form

--- php/form-processors/login-form.php
+++ php/form-processors/login-form.php
@@ -1,25 +1,57 @@
 <!DOCTYPE html>
-<html>
-<head lang="en">
-	<meta charset="UTF-8">
-	<title>Login</title>Login to Red or Green Events Here:</script>
-	<script type ="text/javascript" src ="//cdnjs.cloudfare.com/ajax/lins/query.form/3.51/jquery.form.min.js"></script>
-	<script type ="text/javascript" src ="//ajax.aspnet.cdn.com/ajax/jquery.validate.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="jquery_form.css" />
-</head>
-<body>
-<form id="loginForm">
-	Email:			   <input id="email"           name ="email"           type ="text"    /><br />
-	Password:			<input id="password"        name ="password"        type ="text"    /><br />
-	<button type ="submit">Login</button>
-	<script type ="text/javascript" src ="//ajax.googleapis.com/ajax/libs/jquery.min.js"></script>
-	<script type ="text/javascript" src ="//malsup.github.com/min/jquery.validate/1.11.1/j
-</form>
-</body>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<link rel="icon" href="../../favicon.ico">
+
+		<title>Sign in</title>
+
+			<!-- Bootstrap core CSS -->
+		<link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+
+			<!-- Custom styles for this template -->
+		<link href="signin.css" rel="stylesheet">
+
+
+		<script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+
+	<body>
+
+	<div class="container">
+
+		<form class="form-signin" role="form">
+			<h2 class="form-signin-heading">Please sign in</h2>
+			<label for="inputEmail" class="sr-only">Email address</label>
+			<input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+			<label for="inputPassword" class="sr-only">Password</label>
+			<input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+			<div class="checkbox">
+				<label>
+					<input type="checkbox" value="remember-me"> Remember me
+				</label>
+			</div>
+			<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+		</form>
+
+	</div>
+
+	<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+	</body>
 </html>
 /**
- * Created by PhpStorm.
- * User: Leonard
- * Date: 12/1/2014
- * Time: 5:50 PM
- */ 
+* Created by PhpStorm.
+* User: Leonard
+* Date: 12/1/2014
+* Time: 5:50 PM
+*/
