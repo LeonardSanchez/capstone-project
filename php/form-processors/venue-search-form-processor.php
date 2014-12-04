ad9f6dec8af4f5d90a5dea900f0db785f12573cb
James Mistalski <kimomistalski@gmail.com>
2014-12-04 01:52:11 -0700

    - added try and catch, and CSRF to venue-search-form-processor.php https://app.asana.com/0/19128794071892/19128794071892

--- php/form-processors/venue-search-form-processor.php
+++ php/form-processors/venue-search-form-processor.php
@@ -2,25 +2,39 @@
 session_start();
 // require the needed class to run search against
 require_once("../classes/venue.php");
-
-// require mySQLI
 require_once("/etc/apache2/capstone-mysql/rgevents.php");
-$mysqli = MysqliConfiguration::getMysqli();
+require_once("../forms/csrf.php");
 
-// use filter_input to sanitize the venue name
-$venueName = filter_input(INPUT_GET, 'venue', FILTER_SANITIZE_STRING);
-// once sanitized, the variable is ready to go
+try {
+	// require mySQLI
+	$mysqli = MysqliConfiguration::getMysqli();
 
-// next, grab the mySQL data
-$venues = Venue::getVenueByVenueName($mysqli, $venueName);
+	// verify the form was submitted OK
+	if(@isset($_GET["venue"]) === false) {
+		throw(new RuntimeException("Form variables incomplete or missing"));
+	}
 
-// use a foreach loop to return the data as an array of objects
-foreach($venues as $venue) {
-	echo 	"<p><strong>" . $venue->getVenueName() . "</strong><br />" .
+	// verify the CSRF tokens
+	if(verifyCsrf($_POST["csrfName"], $_POST["csrfToken"]) === false) {
+		throw(new RuntimeException("CSRF tokens incorrect or missing. Make sure cookies are enabled."));
+	}
+	// use filter_input to sanitize the venue name
+	$venueName = filter_input(INPUT_GET, 'venue', FILTER_SANITIZE_STRING);
+	// once sanitized, the variable is ready to go
+
+	// next, grab the mySQL data
+	$venues = Venue::getVenueByVenueName($mysqli, $venueName);
+
+	// use a foreach loop to return the data as an array of objects
+	foreach($venues as $venue) {
+		echo "<p><strong>" . $venue->getVenueName() . "</strong><br />" .
 			$venue->getVenueAddress1() . "<br />" .
 			$venue->getVenueAddress2() . "<br />" .
 			$venue->getVenueCity() . ", " . $venue->getVenueState() . " " . $venue->getVenueZipCode() . "<br />" .
 			$venue->getVenuePhone() . "<br />" .
 			$venue->getVenueWebsite() . "</p>";
+	}
+} catch(Exception $exception) {
+	echo "div class=\"alert alert-danger\" role=\"alert\"><strong>Oh snap!</strong>Unable to search venue: " . $exception->getMessage() . "</div>";
 }
 ?>
