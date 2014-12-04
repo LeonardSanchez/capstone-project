6e1b750f1bd21372fdb19d788a6c811a5b0155ef
Brendan Slevin <bslevin87@gmail.com>
2014-12-03 17:02:59 -0700

    worked with the shopping-cart-form.php

--- php/forms/shopping-cart-form.php
+++ php/forms/shopping-cart-form.php
@@ -6,16 +6,17 @@ session_start();
    <head lang="en">
       <meta charset="UTF-8">
       <title>Shopping Cart</title>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
       <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
       <script type="text/javascript" src="//malsup.github.com/min/jquery.form.min.js"></script>
       <script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script>
       <script type="text/javascript" src="//cdnjs.clooudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
-      <link rel="stylesheet" type="text/css" />
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" />
    </head>
    <body>
-      <form id="shoppingCart" action="transaction.php" method="POST">
+      <form id="shoppingCart" action="../classes/transaction.php" method="POST">
          Ticket Quantity:<select>
+									 <option>0</option>
 									 <option>1</option>
 									 <option>2</option>
 									 <option>3</option>
@@ -26,11 +27,11 @@ session_start();
 									 <option>8</option>
 									 <option>9</option>
 									 <option>10</option>
-								 </select> <input id="quantity" name="quantity" type="integer" /><br />
+								 </select><br />
          <button type="button">Empty Cart</button>
          <button type="button">Update Cart</button>
          <button type="button">Continue Shopping</button>
-         <button type="submit">Checkout</button>
+         <button type="submit" <a href="../checkout-stripe.php">Checkout</a></button>
       </form>
    </body>
 </html>
