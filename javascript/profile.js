cb3b56041ff349c91e14b5c49f533e5dc874a768
Leonard <Suavelen69@gmail.com>
2014-12-04 12:40:16 -0700

    edit-profile.js

--- javascript/profile.js
+++ javascript/profile.js
@@ -1,72 +1,86 @@
-/**
- * Created by Leonard on 12/1/2014.
- */
-<?php
-session_start();
-require_once("/etc/apache2/capstone-mysql/prework.php");
-require_once("../lib/csrf.php");
-require_once("profile.php");
-require_once("Mail.php");
+// validate the form using jQuery
+$(document).ready(function() {
+	// set up the form validation
+	$("#profile").validate({
+		// debug option in jQuery's validator
+		debug: true,
+		// rules that dictate whats (in)valid
+		rules: {
+			FirstName      : {
+				required : true,
+				FirstName: true
+			},
+			LastName       : {
+				required: true,
+				LastName: true
+			},
+			dateOfBirth    : {
+				required   : true,
+				dateOfBirth: true
+			},
+			gender         : {
+				required: true,
+				gender  : true
+			},
+			email          : {
+				required: true,
+				email   : true
+			},
+			password       : {
+				required: true,
+				password: true
+			},
+			confirmPassword: {
+				required: true,
+				equalTo : "password"
+			},
+			// messages that are displayed to the user
+			messages       : {
+				firstName      : "Please enter a valid name in the field",
+				lastName       : "Please enter a valid name in the field",
+				dateOfBirth    : "Please enter a valid date of birth",
+				gender         : "Please enter a valid gender",
+				email          : "Please enter a valid email",
+				password       : "Please enter a valid password",
+				confirmPassword: {
+					// confirmPassword was empty
+					required: "Please confirm the password",
+					// passwords did not match
+					equalTo : "Passwords did not match"
+				}
+			},
+			submitHandler  : function(form) {
+				$(form).ajaxSubmit({
+					type   : "POST",
+					url    : "profile.php",
+					data   : $(form).serialize(),
+					success: function(ajaxOutput) {
+						$("outputArea").css("display", "");
+						$("outputArea").html(ajaxOutput);
 
-try {
-    // verify the form was submitted OK
-    if (@isset($_POST["firstName"]) === false || @isset($_POST["lastName"]) === false || @isset($_POST["dateOfBirth"]) === false || @isset($_POST["gender"]) === false @isset($_POST["email"]) === false) {
-        throw(new RuntimeException("Form variables incomplete or missing"));
-    }
+						if($(".alert-success").length >= 1) {
+							$(form).reset();
+						}
+					}
+				});
+			}
 
-    // verify the CSRF tokens
-    if(verifyCsrf($_POST["csrfName"], $_POST["csrfToken"]) === false) {
-        throw(new RuntimeException("CSRF tokens incorrect or missing. Make sure cookies are enabled."));
-    }
 
-    // create a new object and insert it to mySQL
-    $authToken = bin2hex(openssl_random_pseudo_bytes(16));
-    $profile   = new Profile(null, $_POST["firstName"], $_POST["lastName"], $_POST["dateOfBirth"], $_POST["gender"], $_POST["email"], $authToken, null, null);
-    $mysqli    = MysqliConfiguration::getMysqli();
-    $profile->insert($mysqli);
-
-    // email the user with an activation message
-    $to   = $profile->getEmail();
-    $from = "leonard@cnm.edu";
-
-    // build headers
-    $headers                 = array();
-    $headers["To"]           = $to;
-    $headers["From"]         = $from;
-    $headers["Repy-To"]      = $from;
-    $headers["Subject"]      = $profile->getFirstName() . " " . $profile->getLastName() . " ". $profile->getDateOfBirth . " " . $profile->getGender ." " . $profile->getEmail . "Welcome back to Red or Green Events";
-    $headers["MIME-Version"] = "1.0";
-    $headers["Content-Type"] = "text/html; charset=UTF-8";
-
-    // build message
-    $pageName = end(explode("/", $_SERVER["PHP_SELF"]));
-    $url      = "http://" . $_SERVER["SERVER_NAME"] . $_SERVER["PHP_SELF"];
-    $url      = str_replace($pageName, "activate.php", $url);
-    $url      = "$url?authToken=$authToken";
-    $message  = <<< EOF
-<html>
-    <body>
-        <h1>Welcome back to Red or Green Events!</h1>
-        <hr />
-        <p>Welcome back to Red or Green Events! <a href="$url">$url</a>.</p>
-    </body>
-</html>
-EOF;
-
-    // send the email
-    error_reporting(E_ALL & ~E_STRICT);
-    $mailer =& Mail::factory("sendmail");
-    $status = $mailer->send($to, $headers, $message);
-    if(PEAR::isError($status) === true)
-    {
-        echo "<div class=\"alert alert-danger\" role=\"alert\"><strong>Yo MAMA!</strong> Unable to send mail message:" . $status->getMessage() . "</div>";
-}
-else
-    {
-		 echo "<div class=\"alert alert-success\" role=\"alert\"><strong>Profile Login successful!</strong> Welcome back to Red or Green Events!</div>";
-		 }
-
-} catch(Exception $exception) {
-	echo "<div class=\"alert alert-danger\" role=\"alert\"><strong>Yo MAMA!</strong> Unable to find profile: " . $exception->getMessage() . "</div>";
+		}
+	});
+	/* verifies if the passwords match
+	 * input: n/a
+	 * output: (boolean) true if they matched, false if not */
+	function verifyPasswords() {
+		var firstPassword = document.getElementById("password").value;
+		var secondPassword = document.getElementById("confirmPassword").value;
+		// see if they match and blame the user!
+		if(firstPassword != secondPassword) {
+			alert("Passwords do not match");
+			return (false);
+		}
+		else {
+			return (true);
+		}
 	}
-?>
+})
--- javascript/signUp.js
+++ javascript/signUp.js
@@ -1,69 +1,86 @@
-/**
- * Created by Leonard on 12/1/2014.
- */
-<?php
-session_start();
-require_once("/etc/apache2/capstone-mysql/prework.php");
-require_once("../lib/csrf.php");
-require_once("signup.php");
-require_once("Mail.php");
+// validate the form using jQuery
+$(document).ready(function() {
+	// set up the form validation
+	$("#signUp").validate({
+		// debug option in jQuery's validator
+		debug: true,
+		// rules that dictate whats (in)valid
+		rules: {
+			firstName      : {
+				required : true,
+				FirstName: true
+			},
+			lastName       : {
+				required: true,
+				LastName: true
+			},
+			dateOfBirth    : {
+				required   : true,
+				dateOfBirth: true
+			},
+			gender         : {
+				required: true,
+				gender  : true
+			},
+			email          : {
+				required: true,
+				email   : true
+			},
+			password       : {
+				required: true,
+				password: true
+			},
+			confirmPassword: {
+				required: true,
+				equalTo : "password"
+			},
+			// messages that are displayed to the user
+			messages       : {
+				firstName      : "Please enter a valid name in the field",
+				lastName       : "Please enter a valid name in the field",
+				dateOfBirth    : "Please enter a valid date of birth",
+				gender         : "Please enter a valid gender",
+				email          : "Please enter a valid email",
+				password       : "Please enter a valid password",
+				confirmPassword: {
+					// confirmPassword was empty
+					required: "Please confirm the password",
+					// passwords did not match
+					equalTo : "Passwords did not match"
+				}
+			},
+			submitHandler  : function(form) {
+				$(form).ajaxSubmit({
+					type   : "POST",
+					url    : "sign-up.php",
+					data   : $(form).serialize(),
+					success: function(ajaxOutput) {
+						$("outputArea").css("display", "");
+						$("outputArea").html(ajaxOutput);
 
-try {
-   //verify that the form submitted OK
-}	if (@isset($_POST["firstName"],) === false || @isset($_POST["lastName"]) === false || @isset($_POST["dateOfBirth"]) === false || @isset($_POST["gender"]) === false @isset($_POST["email"] === false {
-		throw(new RuntimeException("Form variables incomplete or missing"));
-	}
-	//verify the CSRF tokens
-	if(verifyCsrf($_POST["csrfName"], $_POST["csrfToken"]) === false) {
-		throw(new RuntimeException("CSRF tokens incorrect or missing. Make sure cookies are enabled"));
+						if($(".alert-success").length >= 1) {
+							$(form).reset();
+						}
+					}
+				});
+			}
+
+
 		}
-	// create a new object and insert it into mySQL
-	$authToken = bin2hex(openssl_random_pseudo_bytes(16));
-	$signup    = new Signup(null, $_POST["firstName"], $_POST["lastName"], $_POST["dateOfBirth"], $_POST["gender"], $_POST["email"], $authToken, null, null);
-	$mysqli    = MysqliConfiguration::getMysqli();
-	$signup->insert(mysqli);
-
-	//email the user with an activation message
-	$to   = $signup->getEmail ();
-	$from = "leonard@cnm.edu";
-
-	//build headers
-	$headers							= array();
-	$headers["To"] 				= $to;
-	$headers["From"]				= $from;
-	$headers["Reply-To"]			= $from;
-	$headers["Subject"] 			= $signup->getFirstName() . " " . $signup->getLastName . ", Activate your Red Or Green Events Login";
-	$headers["MIME-Version"]   = "1.0";
-	$headers["Content-Type"]   = "text/html; charset=UTF-8";
-
-	//build message
-	$pageName = end(explode("/", $_SERVER["SERVER_NAME"] . $_SERVER["PHP_SELF"]));
-	$url		 = "http://" . $_SERVER["SERVER_NAME"] . $_SERVER["PHP_SELF"];
-	$url      = str_replace(#pageName, "activeate.php", $url);
-	$url      = "url?authToken=$authToken";
-	$message  = <<<EOF
-<html>
-	<body>
-		<h1>Welcome to Red or Green Events</h1>
-		<hr />
-		<p>Thank you for signing up with Red or Green Events. Visit the following URL to complete your registration process. <a href="$url">$url</a>.</p>
-	</body>
-</html>
-EOF:
-
-	//send the email
-	error_reporting(E_ALL & ~E_STRICT);
-	$mailer =& Mail ::factory("sendmail");
-	$status = $mailer->send($to, $headers, $message);
-	if (PEAR::isError($status) === true)
-	{
-		echo "<div class=\"alert alert-danger\" role=\"alert\"><strong>YO MAMA!</strong> Unable to send mail message: $status->getMessage(). </div>;
-	}
-		else
-	{
-		echo"<div class=\"alert alert-success\" role="alert\"><strong>Sign up successful!</strong>Welcome to red or Green Events.</div>";
+	});
+	/* verifies if the passwords match
+	 * input: n/a
+	 * output: (boolean) true if they matched, false if not */
+	function verifyPasswords() {
+		var firstPassword = document.getElementById("password").value;
+		var secondPassword = document.getElementById("confirmPassword").value;
+		// see if they match and blame the user!
+		if(firstPassword != secondPassword) {
+			alert("Passwords do not match");
+			return (false);
 		}
-} catch(Exception $exception){
-		echo"<div class=\"alert alert-danger\" role=\"alert\"><strong>YO MAMA!</strong>Unable to sign up: " . $exception->getMessage() . </div>";
+		else {
+			return (true);
+		}
 	}
-	?>
+})
