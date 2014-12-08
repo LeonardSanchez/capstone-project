636756e1c17d8a3b078c62c809b38afedc68e975
James Mistalski <kimomistalski@gmail.com>
2014-12-08 14:17:09 -0700

    - add to cart form https://app.asana.com/0/19128794071892/19128794071892

--- php/forms/add-to-cart-form.php
+++ php/forms/add-to-cart-form.php
@@ -17,18 +17,19 @@ session_start();
 </head>
 <body>
 	<form id="addToCart" action="../form-processors/add-to-cart-form-processor.php" method="post">
-			Ticket Quantity:<select>
-				<option>1</option>
-				<option>2</option>
-				<option>3</option>
-				<option>4</option>
-				<option>5</option>
-				<option>6</option>
-				<option>7</option>
-				<option>8</option>
-				<option>9</option>
-				<option>10</option>
-			</select>
+		<input type="hidden" name="eventId" value="<?php echo $event->getEventId(); ?>" />
+		<select name="quantity">
+			<option value="1">1</option>
+			<option value="2">2</option>
+			<option value="3">3</option>
+			<option value="4">4</option>
+			<option value="5">5</option>
+			<option value="6">6</option>
+			<option value="7">7</option>
+			<option value="8">8</option>
+			<option value="9">9</option>
+			<option value="10">10</option>
+		</select>
 
 		<br />
 		<input id="addToCart" type="button" value="Add to Cart" onclick="addToCart()" />
