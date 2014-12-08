58f062a4a32602ac9ecdce4910f667688cb08878
Brendan Slevin <bslevin87@gmail.com>
2014-12-08 12:58:39 -0700

    shopping cart work

--- php/forms/shopping-cart-form.php
+++ php/forms/shopping-cart-form.php
@@ -32,11 +32,11 @@ session_start();
 								  </select>
 			Ticket Price Each</h3>
 			<br />
-			<input id="empty" type="button" value="Empty Cart" onclick="emptyCart();" />
-			<input id="remove" type="button" value="Remove from" onclick="removeFromCart();"/>
-			<input id="update" type="button" value="Update Cart" onclick="updateCart();" />
-			<input id="continueShopping" type="button" value="Continue Shopping" onclick="continueShopping();" />
-			<input id="checkout" type="button" value="Checkout" onclick="checkout();" />
+			<input id="empty" type="button" value="Empty Cart" onclick="emptyCart()" />
+			<input id="remove" type="button" value="Remove from" onclick="removeFromCart()"/>
+			<input id="update" type="button" value="Update Cart" onclick="updateCart()" />
+			<input id="continueShopping" type="button" value="Continue Shopping" onclick="continueShopping()" />
+			<input id="checkout" type="button" value="Checkout" onclick="checkout()" />
       </form>
    </body>
 </html>
