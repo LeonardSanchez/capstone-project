580495443db68c72dfc76f15c1ca6645d98c11d2
Leonard <Suavelen69@gmail.com>
2014-12-08 13:23:01 -0700

    add-to-cart

---
+++ php/classes/add-to-cart.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head lang="en">
+	<meta charset="UTF-8">
+	<title>Shopping Cart</title>
+	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script type="text/javascript" src="//malsup.github.com/min/jquery.form.min.js"></script>
+	<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script>
+	<script type="text/javascript" src="//cdnjs.clooudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+	<script type="text/javascript" src="/javascript/shopping-cart.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" />
+</head>
+<body>
+<form id="addToCart" action="../classes/transaction.php" method="POST">
+	<h1>Add to Cart</h1> <br /> <br />
+	<h3>Event Name
+		Ticket Quantity:<select>
+			<option>0</option>
+			<option>1</option>
+			<option>2</option>
+			<option>3</option>
+			<option>4</option>
+			<option>5</option>
+			<option>6</option>
+			<option>7</option>
+			<option>8</option>
+			<option>9</option>
+			<option>10</option>
+		</select>
+		Ticket Price Each</h3>
+	<br />
+	<input id="addToCart" type="button" value="Add to Cart" onclick="addToCart"(); />
+	<input id="empty" type="button" value="Empty Cart" onclick="emptyCart();" />
+	<input id="remove" type="button" value="Remove from" onclick="removeFromCart"();/>
+	<input id="update" type="button" value="Update Cart" onclick="updateCart();" />
+	<input id="continueShopping" type="button" value="Continue Shopping" onclick="continueShopping();" />
+	<input id="checkout" type="button" value="Checkout" onclick="checkout();" />
+</form>
+</body>
+</html>
---
+++ php/form-processors/add-to-cart-form.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Add to cart form
+ *
+ * This form will be used to add any quantity of tickets to shopping cart before checkout
+ *
+ */
+// Make sure to start a session
+session_start();
+// require the transaction class so we can pull the ticket id which will have the event price and such
+require_once("../classes/event.php");
+// require to connect to our server I believe
+require_once("/etc/apache2/capstone-mysql/rgevents.php");
+// require for the csrf protection
+require_once("../forms/csrf.php");
+
+// assign the session to our cart
+$_SESSION["cart"]=$cart;
+
+$cart = array(
+
+);
+if (quantity < 0){
+	throw(RangeException("This is a negative quantity."));
+	(qunatity === 0);
+		unset:
+}
+
+// empty cart
+unset($_SESSION["cart"]);
+?>
+<form method="post" action="add-to-cart.php">
+	<input type="hidden" name="eventId" value="<?php echo $event->getEventId(); ?>" />
+	<select name="quantity">
+		<option value="1" selected>1</option>
+		<option value="2">2</option>
+		<option value="3">3</option>
+		<option value="4">4</option>
+		<option value="5">5</option>
+		<option value="6">6</option>
+		<option value="7">7</option>
+		<option value="8">8</option>
+		<option value="9">9</option>
+		<option value="10">10</option>
+	</select>
+	Ticket Price Each</h3>
+	<br />
+
+	</select>
+	<button type="submit">Add to Cart</button>
+	<h3>Add to Cart</h3>
+	<input id="Add to cart" type="button" value="Add to cart" onclick="addToCart();"/>
+	<input id="empty" type="button" value="Empty Cart" onclick="emptyCart();" />
+	<input id="remove" type="button" value="Remove from" onclick="removeFromCart();"/>
+	<input id="update" type="button" value="Update Cart" onclick="updateCart();" />
+	<input id="continueShopping" type="button" value="Continue Shopping" onclick="continueShopping();" />
+	<input id="checkout" type="button" value="Checkout" onclick="checkout();" />
+
+
+
+
+</form>
+
+
