b61cac20380530a98551cb7304e2fe219d02a2df
James Mistalski <kimomistalski@gmail.com>
2014-12-08 14:04:06 -0700

    - add to cart form https://app.asana.com/0/19128794071892/19128794071892

---
+++ javascript/add-to-cart.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Kimo on 12/8/2014.
+ */
--- php/forms/add-to-cart-form.php
+++ php/forms/add-to-cart-form.php
@@ -5,40 +5,34 @@ session_start();
 <html>
 <head lang="en">
 	<meta charset="UTF-8">
-	<title>Shopping Cart</title>
-	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-	<script type="text/javascript" src="//malsup.github.com/min/jquery.form.min.js"></script>
-	<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script>
-	<script type="text/javascript" src="//cdnjs.clooudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
-	<script type="text/javascript" src="/javascript/shopping-cart.js"></script>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" />
+	<title>Add to Cart</title>
+	<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet" />
+	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+	<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/jquery.validate.min.js"></script>
+	<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/additional-methods.min.js"></script>
+	<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="../../javascript/add-to-cart.js"></script>
+
 </head>
 <body>
-<form id="addToCart" action="../classes/transaction.php" method="POST">
-	<h1>Add to Cart</h1> <br /> <br />
-	<h3>Event Name
-		Ticket Quantity:<select>
-			<option>0</option>
-			<option>1</option>
-			<option>2</option>
-			<option>3</option>
-			<option>4</option>
-			<option>5</option>
-			<option>6</option>
-			<option>7</option>
-			<option>8</option>
-			<option>9</option>
-			<option>10</option>
-		</select>
-		Ticket Price Each</h3>
-	<br />
-	<input id="addToCart" type="button" value="Add to Cart" onclick="addToCart"(); />
-	<input id="empty" type="button" value="Empty Cart" onclick="emptyCart();" />
-	<input id="remove" type="button" value="Remove from" onclick="removeFromCart"();/>
-	<input id="update" type="button" value="Update Cart" onclick="updateCart();" />
-	<input id="continueShopping" type="button" value="Continue Shopping" onclick="continueShopping();" />
-	<input id="checkout" type="button" value="Checkout" onclick="checkout();" />
-</form>
+	<form id="addToCart" action="../form-processors/add-to-cart-form-processor.php" method="post">
+			Ticket Quantity:<select>
+				<option>1</option>
+				<option>2</option>
+				<option>3</option>
+				<option>4</option>
+				<option>5</option>
+				<option>6</option>
+				<option>7</option>
+				<option>8</option>
+				<option>9</option>
+				<option>10</option>
+			</select>
+
+		<br />
+		<input id="addToCart" type="button" value="Add to Cart" onclick="addToCart()" />
+
+	</form>
 </body>
 </html>
