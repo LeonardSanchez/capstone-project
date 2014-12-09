d095195cf42cefa6887cf5a0d672a13041ceaea2
James Mistalski <kimomistalski@gmail.com>
2014-12-09 08:34:06 -0700

    - add to cart form processor coming along https://app.asana.com/0/19128794071892/19128794071892

--- php/form-processors/shopping-cart-form-processor.php
+++ php/form-processors/shopping-cart-form-processor.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 /**
 * Shopping cart form
 *
@@ -6,14 +7,19 @@
 *
  * Created by Brendan Slevin
 */
-// Make sure to start a session
-session_start();
 // require the transaction class so we can pull the ticket id which will have the event price and such
 require_once("../classes/event.php");
 // require to connect to our server I believe
 require_once("/etc/apache2/capstone-mysql/rgevents.php");
 // require for the csrf protection
 require_once("../forms/csrf.php");
+
+	$csrfName = isset($_POST["csrfName"]) ? $_POST["csrfName"] : false;
+	$csrfToken = isset($_POST["csrfToken"]) ? $_POST["csrfToken"] : false;
+
+	if(verifyCsrf($_POST["csrfName"], $_POST["csrfToken"]) === false) {
+		throw(new Exception("external source violation"));
+	}
 
 // assign the session to our cart
 $_SESSION["cart"]=$cart;
