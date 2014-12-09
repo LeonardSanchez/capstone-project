204aa06d643b4548b616b10328fdef156fa474e7
Brendan Slevin <bslevin87@gmail.com>
2014-12-09 13:41:24 -0700

    worked on the the shopping cart form processor https://app.asana.com/0/19128794071914/21140848412754

--- php/form-processors/shopping-cart-form-processor.php
+++ php/form-processors/shopping-cart-form-processor.php
@@ -1,12 +1,13 @@
+
 <?php
-session_start();
 /**
-* Shopping cart form
-*
+ * Shopping cart form
+ *
  * This form will be used to collect quantity of tickets desired and allow the user to see a preview of the order before checkout
-*
+ *
  * Created by Brendan Slevin
-*/
+ */
+session_start();
 // require the transaction class so we can pull the ticket id which will have the event price and such
 require_once("../classes/event.php");
 // require to connect to our server I believe
@@ -14,8 +15,9 @@ require_once("/etc/apache2/capstone-mysql/rgevents.php");
 // require for the csrf protection
 require_once("../forms/csrf.php");
 
-	$csrfName = isset($_POST["csrfName"]) ? $_POST["csrfName"] : false;
-	$csrfToken = isset($_POST["csrfToken"]) ? $_POST["csrfToken"] : false;
+// csrf
+$csrfName = isset($_POST["csrfName"]) ? $_POST["csrfName"] : false;
+$csrfToken = isset($_POST["csrfToken"]) ? $_POST["csrfToken"] : false;
 
 	if(verifyCsrf($_POST["csrfName"], $_POST["csrfToken"]) === false) {
 		throw(new Exception("external source violation"));
@@ -29,27 +31,27 @@ $cart = array(
 
 );
 
+// check to see how many remaining tickets there are for the event
+$ticket = Ticket::getTicketByEventId($mysqli, $eventId);
+
+// make sure the quantity selected is a integer
+$qty = filter_var($qty, FILTER_VALIDATE_INT);
+
+// add ticket to cart
 if ($qty <= 0){
 	throw(RangeException("This is a negative quantity."));
-} else if ()
-if ()
+}
+
+if ($qty > 0){
+	array_push($cart, $qty);
+}
+
+// remove ticket/s
+
+
 // empty cart
 unset($_SESSION["cart"]);
 
-$dropDown = "<select name="ticketQuantity">
-								    <option value="1" selected>1</option>
-									 <option value="2">2</option>
-									 <option value="3">3</option>
-									 <option value="4">4</option>
-									 <option value="5">5</option>
-									 <option value="6">6</option>
-									 <option value="7">7</option>
-									 <option value="8">8</option>
-									 <option value="9">9</option>
-									 <option value="10">10</option>
-								  </select>";
-// some potential
-str_replace(">".$cart[$eventId],"selected>"$cart[$eventId],$dropdown);
 
 
 ?>
