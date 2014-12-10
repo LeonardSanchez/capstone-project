6176571422bd9f899bd4ae30926389cc47f30e82
Brendan Slevin <bslevin87@gmail.com>
2014-12-10 15:42:28 -0700

    did some work on the checkout form requiring the shopping cart data https://app.asana.com/0/19128794071914/21140848412756

--- php/forms/checkout-stripe.php
+++ php/forms/checkout-stripe.php
@@ -1,3 +1,8 @@
+<?php
+require_once("shopping-cart-form.php");
+require_once("../form-processors/shopping-cart-form-processor.php");
+?>
+
 <!DOCTYPE html>
 <html>
 <head lang="en">
@@ -10,19 +15,24 @@
 	<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/additional-methods.min.js"></script>
 	<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="https://js.stripe.com/v2/"></script>
+	<script type="text/javascript" src="../../javascript/checkout.js"></script>
 	<link rel="stylesheet" type="text/css" />
 </head>
 <body>
 	<form id="checkout-form" action="checkout-stripe.php" method="post">
+		<label>Cart Total</label><br />
+		<?php ?>
 		<label>Card Number</label><br />
 		<input type="text" size="20" autocomplete="off"><br />
-		<span>Please enter the number without spaces or hyphens.</span><br />
+		<span>Please enter the number without spaces or hyphens.</span><br /><br />
 		<label>CVC</label><br />
-		<input type="text" size="4" autocomplete="off"><br />
+		<input type="text" size="4" autocomplete="off"><br /><br />
 		<label>Expiration (MM/YYYY)</label><br />
 		<input type="text" size="2">
 		<span> / </span>
-		<input type="text" size="4"><br />
+		<input type="text" size="4"><br /><br />
+		<label>Email Address</label><br />
+		<input type="email" autocomplete="off"><br />
 		<div id="payment-errors"></div>
 	</form>
 </body>
