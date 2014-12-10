120f420c147c6a163a88e9a69f4e55e1b7370c4d
James Mistalski <kimomistalski@gmail.com>
2014-12-10 00:16:34 -0700

    - added library of glyphicons to image directory https://app.asana.com/0/19128794071892/19128794071892

--- index.php
+++ index.php
@@ -88,6 +88,7 @@ session_start();
 </div>
 
 
+
 <!-- Bootstrap core JavaScript
  ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
