062a0882aa1a22e9f53b214cb80b59648548228a
Brendan Slevin <bslevin87@gmail.com>
2014-12-10 14:59:52 -0700

    did some work on the checkout form https://app.asana.com/0/19128794071914/21140848412756

--- php/forms/checkout-stripe.php
+++ php/forms/checkout-stripe.php
@@ -3,29 +3,26 @@
 <head lang="en">
 	<meta charset="UTF-8">
 	<title>Checkout</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-	<script type="text/javascript" src="//malsup.github.com/min/jquery.form.min.js"></script>
-	<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script>
-	<script type="text/javascript" src="//cdnjs.clooudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+	<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet" />
+	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+	<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/jquery.validate.min.js"></script>
+	<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/additional-methods.min.js"></script>
+	<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="https://js.stripe.com/v2/"></script>
 	<link rel="stylesheet" type="text/css" />
 </head>
 <body>
-	<h1>My shopping cart</h1>
-		<h3>
-			<ol>
-				<li>Item 1</li>
-				<li>Item 2</li>
-			</ol>
-		</h3>
-	<form action="" method="POST">
-		<script
-			src="https://checkout.stripe.com/checkout.js" class="stripe-button"
-			data-key="pk_test_zqXDIk9h00Tk0kLxXw0UrENB"
-			data-amount="2000"
-			data-name="RG Events"
-			data-description="Test Ticket"
-			data-image="/128x128.png">
-		</script>
+	<form id="checkout-form" action="checkout-stripe.php" method="post">
+		<label>Card Number</label>
+		<input type="text" size="20" autocomplete="off">
+		<span>Please enter the number without spaces or hyphens.</span>
+		<label>CVC</label>
+		<input type="text" size="4" autocomplete="off">
+		<label>Expiration (MM/YYYY)</label>
+		<input type="text" size="2">
+		<span> / </span>
+		<input type="text" size="4"
+		<div id="payment-errors"></div>
 	</form>
 </body>
