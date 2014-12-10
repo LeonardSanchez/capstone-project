96969122af775801e7954f54254e6aaffdb66d9c
James Mistalski <kimomistalski@gmail.com>
2014-12-10 08:08:29 -0700

    - signup-form-processor.php not processing correctly, getting an error that can't execute mySQL statement https://app.asana.com/0/19128794071892/19128794071892

--- php/form-processors/signup-form-processor.php
+++ php/form-processors/signup-form-processor.php
@@ -8,7 +8,7 @@ require_once("../classes/user.php");
 require_once("../classes/profile.php");
 require_once("../forms/csrf.php");
 require_once("../forms/signup-form.php");
-// require_once("Mail.php");
+require_once("Mail.php");
 
 try {
 	// require mySQLI
@@ -38,46 +38,47 @@ try {
 	$profile = new Profile(null, $this->user->getUserId(), $_POST["firstName"], $_POST["lastName"], $_POST["dateOfBirth"], $_POST["gender"]);
 	$profile->insert($mysqli);
 
-//	// email the user with an activation message
-//	$to 	= $user->getEmail();
-//	// FIXME: need to create an email from our server to replace personal email below
-//	$from	= "james.mistalski@gmail.com";
-//
-//	// build headers
-//	$headers 					= array();
-//	$headers["To"] 			= $to;
-//	$headers["From"] 			= $from;
-//	$headers["Reply-To"] 		= $from;
-//	$headers["Subject"] 		= $profile->getFirstName() . " " . $profile->getLastName() . ", Activate your RGEvents Login";
-//	$headers["MIME-Version"] = "1.0";
-//	$headers["Content-Type"] = "text/html; charset=UTF-8";
-//
-//	// build message
-//	$pageName 	= end(explode("/", $_SERVER["PHP_SELF"]));
-//	$url 			= "http://" . $_SERVER["https://bootcamp-coders.cnm.edu"] . $_SERVER["PHP_SELF"];
-//	$url 			= str_replace($pageName, "activate.php", $url);
-//	$url 			= "$url?authToken=$authToken";
-//	$message 	= <<< EOF
-//<html>
-//    <body>
-//        <h1>Welcome to Red or Green Events!</h1>
-//        <hr />
-//        <p>Thank you for signing up for a Red or Green Events Login. Visit the following link to complete your registration process: <a href="$url">$url</a>.</p>
-//    </body>
-//</html>
-//EOF;
-//	// send the email
-//	error_reporting(E_ALL & ~E_STRICT);
-//	$mailer =& Mail::factory("sendmail");
-//	$status = $mailer->send($to, $headers, $message);
-//	if(PEAR::isError($status) === true)
-//	{
-//		echo "<div class=\"alert alert-danger\" role=\"alert\"><strong>Oh snap!</strong> Unable to send mail message:" . $status->getMessage() . "</div>";
-//	}
-//	else
-//	{
+	// email the user with an activation message
+	$to 	= $user->getEmail();
+	// FIXME: need to create an email from our server to replace personal email below
+	$from	= "james.mistalski@gmail.com";
+
+	// build headers
+	$headers 					= array();
+	$headers["To"] 			= $to;
+	$headers["From"] 			= $from;
+	$headers["Reply-To"] 		= $from;
+	$headers["Subject"] 		= $profile->getFirstName() . " " . $profile->getLastName() . ", Activate your RGEvents Login";
+	$headers["MIME-Version"] = "1.0";
+	$headers["Content-Type"] = "text/html; charset=UTF-8";
+
+	// build message
+	$pageName 	= end(explode("/", $_SERVER["PHP_SELF"]));
+	$url 			= "http://" . $_SERVER["https://bootcamp-coders.cnm.edu"] . $_SERVER["PHP_SELF"];
+	$url 			= str_replace($pageName, "activate.php", $url);
+	$url 			= "$url?authToken=$authToken";
+	$message 	= <<< EOF
+<html>
+    <body>
+        <h1>Welcome to Red or Green Events!</h1>
+        <hr />
+        <p>Thank you for signing up for a Red or Green Events Login. Visit the following link to complete your registration process: <a href="$url">$url</a>.</p>
+    </body>
+</html>
+EOF;
+	// send the email
+	error_reporting(E_ALL & ~E_STRICT);
+	$mailer =& Mail::factory("sendmail");
+	$status = $mailer->send($to, $headers, $message);
+	if(PEAR::isError($status) === true)
+	{
+		echo "<div class=\"alert alert-danger\" role=\"alert\"><strong>Oh snap!</strong> Unable to send mail message:" . $status->getMessage() . "</div>";
+	}
+	else
+	{
+
 	echo "<div class=\"alert alert-success\" role=\"alert\"><strong>Sign up successful!</strong> Please check your Email to complete the signup process.</div>";
-//}
+}
 
 } catch(Exception $exception) {
 	echo "<div class=\"alert alert-danger\" role=\"alert\"><strong>Oh snap!</strong> Unable to sign up: " . $exception->getMessage() . "</div>";
