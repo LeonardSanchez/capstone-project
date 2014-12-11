db5ebf05e7f36b39a45d072f723369ac7f05bad3
James Mistalski <kimomistalski@gmail.com>
2014-12-11 10:51:44 -0700

    - updated stripe with image https://app.asana.com/0/19128794071892/19128794071892

--- stripe-library/stripe potential/stripe1.php
+++ stripe-library/stripe potential/stripe1.php
@@ -13,9 +13,9 @@ $priceInCents = $cartTotal * 100;
     src="https://checkout.stripe.com/checkout.js" class="stripe-button"
     data-key="pk_test_6pRNASCoBOKtIshFeQd4XMUh"
     data-amount="<?php echo $priceInCents; ?>"
-    data-name="RG Events"
+    data-name="RGEvents"
     data-description=""
-    data-image="../images/redgreenchilelarge.jpg">
+    data-image="../../images/redgreenchilelarge.jpg">
   </script>
 </form>
 </html>
