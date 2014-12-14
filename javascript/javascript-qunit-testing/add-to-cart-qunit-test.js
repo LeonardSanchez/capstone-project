37cead2833d19525c1c930026f61530a9d5ee047
Leonard <Suavelen69@gmail.com>
2014-12-14 16:09:39 -0700

    FuncUnit tests .js and .php. Created a new php directory for the  tests

--- javascript/javascript-qunit-testing/add-to-cart-qunit-test.js
+++ javascript/javascript-qunit-testing/add-to-cart-qunit-test.js
@@ -5,27 +5,28 @@ module("tabs", {
 		}
 });
 // global variables for form values
-var VALID_
-var VALID_
-var VALID_
-var VALID_
-var VALID_
-var VALID_
-
+var VALID_EVENTID = "149";
+var VALID_EVENTNAME = "A Christmas Carol";
+var VALID_EVENTDATETIME = "12/09/2015";
+var VALID_TICKETPRICE = "$25.00";
+var VALID_QUANTITY = "5";
 // define a function to perform the actual unit tests
 function testValidFields() {
 			// fill in the form values
-			F("").visible(function() {
-					this.type(VALID_);
+			F("#eventId").visible(function() {
+					this.type(VALID_EVENTID);
 			});
-			F("").visible(function() {
-					this.type(VALID_);
+			F("#eventName").visible(function() {
+					this.type(VALID_EVENTNAME);
 			});
-			F("").visible(function() {
-					this.type(VALID_);
+			F("#eventDateTime").visible(function() {
+					this.type(VALID_EVENTDATETIME);
 			});
-			F("").visible(function() {
-					this.type(VALID_);
+			F("#ticketPrice").visible(function() {
+					this.type(VALID_TICKETPRICE);
+			});
+			F("#ticketQuanitiy").visible(function(){
+					this.type(VALID_TICKETQUANTITY);
 			});
 			// click the button once all the fields are filled in
 			F("addToCartSubmit").visible(function() {
