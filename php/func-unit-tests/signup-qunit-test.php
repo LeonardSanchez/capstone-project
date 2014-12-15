cd5512312c748d54f6ff253c52c3d0b9a8ec6dc5
Brendan Slevin <bslevin87@gmail.com>
2014-12-15 16:13:55 -0700

    messing with func unit test https://app.asana.com/0/19128794071914/19128794071914

--- php/func-unit-tests/signup-qunit-test.php
+++ php/func-unit-tests/signup-qunit-test.php
@@ -6,11 +6,7 @@
 					<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 					<script type="text/javascript" src="//code.jquery.com/qunit/qunit-1.15.0.js"></script>
 					<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/FuncUnit/2.0.4/funcunit.min.js"></script>
-<<<<<<< HEAD:php/func-unit-tests/signup-qunit-test.php
 					<script type="text/javascript" src="../../javascript/javascript-qunit-testing/sign-up-qunit-test.js"></script>
-=======
-					<script type="text/javascript" src="signUp-test.js"></script>
->>>>>>> =:php/FuncUnit-tests.php/signup-qunit-test.php
 					<title>Sign Up Test</title>
 		</head>
 		<body>
