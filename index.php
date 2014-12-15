86926fba7b7d06760c6dc5e71e297f88b4926c39
James Mistalski <kimomistalski@gmail.com>
2014-12-15 15:41:36 -0700

    - formatting front end of pages https://app.asana.com/0/19128794071892/19128794071892

--- index.php
+++ index.php
@@ -82,7 +82,7 @@ require_once("php/forms/csrf.php");
 
 		<div class="container-fluid">
 			<div class="row">
-				<div class="col-xs-6 col-md-3" style="background-color: ActiveBorder">
+				<div class="col-xs-6 col-md-2" style="background-color: ActiveBorder">
 					<ul class="nav nav-sidebar" id="leftnav1">
 						<li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
 						<br>
@@ -116,8 +116,33 @@ require_once("php/forms/csrf.php");
 					</ul>
 				</div>
 
-				<div class="col-xs-12 col-md-9" id="pageContent">
-					
+				<div class="col-xs-12 col-md-10" id="indexContent">
+					<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+						<ol class="carousel-indicators">
+							<li data-target="#carousel-example-generic" data-slide-to="0"></li>
+							<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+							<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+						</ol>
+						<div class="carousel-inner" role="listbox">
+							<div class="item">
+								<img data-src="holder.js/1140x500/auto/#777:#555/text:First slide" alt="First slide" data-holder-rendered="true">
+							</div>
+							<div class="item">
+								<img data-src="holder.js/1140x500/auto/#666:#444/text:Second slide" alt="Second slide">
+							</div>
+							<div class="item">
+								<img data-src="holder.js/1140x500/auto#555:#333/text:Third slide" alt="Third slide [1140x500]">
+							</div>
+						</div>
+						<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+							<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+							<span class="sr-only">Previous</span>
+						</a>
+						<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+							<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+							<span class="sr-only">Next</span>
+						</a>
+					</div>
 				</div>
 			</div>
 		</div>
