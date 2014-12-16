49439287ebfc3c5ce860c77a1194bbf19e5106e3
Brendan Slevin <bslevin87@gmail.com>
2014-12-16 13:45:06 -0700

    changed the sign in style a bit https://app.asana.com/0/19128794071914/19128794071914

--- php/forms/login-form.php
+++ php/forms/login-form.php
@@ -16,28 +16,37 @@ require_once("csrf.php");
 	<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/additional-methods.min.js"></script>
 	<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="../../javascript/login.js"></script>
-	<!-- Bootstrap core CSS -->
-	<link href="../../bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-	<!-- Custom styles for this template -->
-	<link href="../../bootstrap/dist/css/signin.css" rel="stylesheet">
+	<link type="text/css" rel="stylesheet" href="/site-css.css" />
 </head>
 <body>
-	<form id="loginForm" method="post" action="../form-processors/login-form-processor.php">
-		<?php echo generateInputTags(); ?>
+<div class="container">
+	<div class="col-lg-6">
 
-		<div class="container">
-				<h3 class="login-section">Please Login</h3>
+		<form class="form-signin" id="loginForm" method="post" action="../form-processors/login-form-processor.php" novalidate="novalidate">
+			<input name="csrfName" type="hidden" value="csrfName163826737">
+			<input name="csrfToken" type="hidden" value="84a0778fd3f5ef5147073ad098bbb5f7dbc204b8c422a17babf7e94a88764491183fa6a2ea8fc44c71495ccf240646539ccf90780f3c652875dd4f18a7e3aa9d">
+			<div class="">
+				<h3 class="form-signin-heading">Please Login</h3>
 				<label for="email" class="sr-only">Email address</label>
 				<input type="text" id="email" name="email" class="form-control" placeholder="Login with your email address">
 				<label for="password" class="sr-only">Password</label>
 				<input type="password" id="password" name="password" class="form-control" placeholder="Password">
 
-				<button id="loginSubmit" class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+				<button id="loginSubmit" class="btn btn-lg btn-primary" type="submit">Login</button>
 
-		</div>
-	</form>
+			</div>
+		</form>
 
-	<p id="outputLogin"></p>
+		<p id="outputLogin"></p>
+
+	</div>
+
+</div>
+
+<p id="outputLogin"></p>
+
+
+
 
 </body>
 
